534ed04b7952825352f98d9f81cc726e5f3785ff
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-22 03:45:31 +0530

    Update EmployeeRepositoryTest.java

--- src/test/java/com/example/employee_api/EmployeeRepositoryTest.java
+++ src/test/java/com/example/employee_api/EmployeeRepositoryTest.java
@@ -5,14 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
-@ActiveProfiles("test")
 class EmployeeRepositoryTest {
 
     @Autowired
