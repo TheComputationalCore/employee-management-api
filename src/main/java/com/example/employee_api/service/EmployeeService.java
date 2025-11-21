c7dfa5833eaa0b936d07aeede5e16cd48e4cba88
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-22 03:01:28 +0530

    Create EmployeeService.java

---
+++ src/main/java/com/example/employee_api/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.example.employee_api.service;
+
+import com.example.employee_api.exception.EmployeeNotFoundException;
+import com.example.employee_api.model.Employee;
+import com.example.employee_api.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(String id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
+    }
+}
