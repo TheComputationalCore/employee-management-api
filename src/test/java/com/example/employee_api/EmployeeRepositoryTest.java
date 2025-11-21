d3483cc65898793a6f763e588b2ea8dcd0f2b976
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-22 03:33:16 +0530

    Update EmployeeRepositoryTest.java

--- src/test/java/com/example/employee_api/EmployeeRepositoryTest.java
+++ src/test/java/com/example/employee_api/EmployeeRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 import java.util.List;
 
